6eafb30f92b74a5d5d08ceb0a9971aae98a03a25
MersenneInteger <zachjohnsonsoftware@gmail.com>
2020-04-08 08:07:12 -0500

    add CTimer to connectAsync

--- TCPClient/TCPClient/TCPClientSsharp.cs
+++ TCPClient/TCPClient/TCPClientSsharp.cs
@@ -9,9 +9,13 @@ namespace TCP_Client
     public class TCPClientSsharp
     {
         #region Properties
+
         private TCPClient _tcpClient;
         private bool _initialized { get; set; }
         private bool _debug { get; set; }
+        private bool _manualDisconnect { get; set; }
+        private CTimer _reconnectTimer;
+
         #endregion
 
         #region Delegates
@@ -33,6 +37,14 @@ namespace TCP_Client
         #endregion
 
         #region S+ Methods
+
+        /// <summary>
+        /// initialize tcp client and subscribe to receive socket status change events
+        /// return 1 to s+ if successful, else 0
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="bufferSize"></param>
         public void Initialize(string ip, uint port, uint bufferSize)
         {
             _tcpClient = new TCPClient(ip, (int)port, (int)bufferSize);
@@ -41,6 +53,7 @@ namespace TCP_Client
             if (_tcpClient.PortNumber > 0 && _tcpClient.AddressClientConnectedTo != string.Empty)
             {
                 _initialized = true;
+                _manualDisconnect = false;
                 InitializedStatus(Convert.ToUInt16(_initialized));
                 CrestronConsole.PrintLine("TCPClient initialized: IP: {0}, Port: {1}",
                             _tcpClient.AddressClientConnectedTo, _tcpClient.PortNumber);
@@ -52,6 +65,9 @@ namespace TCP_Client
             }
         }
 
+        /// <summary>
+        /// connect to server and listen for data
+        /// </summary>
         public void Connect()
         {
             SocketErrorCodes err = new SocketErrorCodes();
@@ -59,9 +75,8 @@ namespace TCP_Client
             {
                 try
                 {
-                    err = _tcpClient.ConnectToServer();
-                    if (err == 0)
-                        err = _tcpClient.ReceiveDataAsync(OnDataReceiveEventCallback);
+                    _manualDisconnect = false;
+                    err = _tcpClient.ConnectToServerAsync(ConnectToServerCallback);
                     Debug("Connection attempt: " +_tcpClient.AddressClientConnectedTo, ErrorLevel.Notice, err.ToString());
                 }
                 catch (Exception e)
@@ -75,11 +90,15 @@ namespace TCP_Client
             }
         }
 
+        /// <summary>
+        /// Disconnect from server
+        /// </summary>
         public void Disconnect()
         {
             SocketErrorCodes err = new SocketErrorCodes();
             try
             {
+                _manualDisconnect = true;
                 _tcpClient.Dispose();
                 err = _tcpClient.DisconnectFromServer();
                 Debug("Disconnect attempt: " + _tcpClient.AddressClientConnectedTo, ErrorLevel.Notice, err.ToString());
@@ -90,18 +109,28 @@ namespace TCP_Client
             }
         }
 
+        /// <summary>
+        /// enable logging to ErrorLog
+        /// </summary>
         public void EnableDebug()
         {
             _debug = true;
             CrestronConsole.PrintLine("Debug Enabled");
         }
 
+        /// <summary>
+        /// disable logging to ErrorLog
+        /// </summary>
         public void DisableDebug()
         {
             _debug = false;
             CrestronConsole.PrintLine("Debug Disabled");
         }
 
+        /// <summary>
+        /// send data to server
+        /// </summary>
+        /// <param name="tx">data</param>
         public void DataTransmit(SimplSharpString tx)
         {
             var err = new SocketErrorCodes();
@@ -114,24 +143,56 @@ namespace TCP_Client
 
         #region Private Methods
 
+        /// <summary>
+        /// if client not connected, connect
+        /// </summary>
+        /// <param name="tcpClient"></param>
+        private void ConnectToServerCallback(TCPClient tcpClient)
+        {
+            if (_tcpClient.ClientStatus != SocketStatus.SOCKET_STATUS_CONNECTED)
+                TryReconnect();
+        }
+
+        /// <summary>
+        /// retry connection every 10s
+        /// </summary>
+        private void TryReconnect()
+        {
+            if (!_manualDisconnect)
+            {
+                Debug("Attempting to connect...", ErrorLevel.None, string.Empty);
+                _reconnectTimer = new CTimer(o => { _tcpClient.ConnectToServerAsync(ConnectToServerCallback); }, 10000);
+            }
+        }
+
+        /// <summary>
+        /// event that triggers when socket status changes, send status to s+
+        /// </summary>
+        /// <param name="tcpClient">tcp client</param>
+        /// <param name="sockStatus">socket status code</param>
         private void OnSocketStatusChange(TCPClient tcpClient, SocketStatus sockStatus)
         {
             if(_sockStatusDict.ContainsKey(sockStatus))
                 ConnectionStatus(sockStatus.ToString(), _sockStatusDict[sockStatus]);
+
             if (sockStatus == SocketStatus.SOCKET_STATUS_CONNECTED)
+            {
+                _tcpClient.ReceiveDataAsync(OnDataReceiveEventCallback);
                 ConnectedFbStatus(1);
+                _manualDisconnect = false;
+            }
             else
             {
                 ConnectedFbStatus(0);
-                if (sockStatus == SocketStatus.SOCKET_STATUS_CONNECT_FAILED
-                    || sockStatus == SocketStatus.SOCKET_STATUS_BROKEN_REMOTELY
-                    || sockStatus == SocketStatus.SOCKET_STATUS_BROKEN_LOCALLY)
-                {
-                    Connect();
-                }
+                TryReconnect();
             }
         }
 
+        /// <summary>
+        /// create buffer for incoming data and pass to S+
+        /// </summary>
+        /// <param name="client">tcp client</param>
+        /// <param name="bytes">buffer size</param>
         private void OnDataReceiveEventCallback(TCPClient client, int bytes)
         {
             byte[] rxBuffer;
@@ -147,6 +208,12 @@ namespace TCP_Client
 
         private enum ErrorLevel { Notice, Warning, Error, None }
 
+        /// <summary>
+        /// Set Debug behavior
+        /// </summary>
+        /// <param name="msg">message to print to console</param>
+        /// <param name="errLevel">set error severity</param>
+        /// <param name="errCode">socket code status to send to s+</param>
         private void Debug(string msg, ErrorLevel errLevel, string errCode)
         {
             if (_debug)
